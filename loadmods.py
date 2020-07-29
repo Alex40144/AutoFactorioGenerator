001a703f1f65c60e7e644327f6d6867449f07146
Alex40144 <giddyraccoon@gmail.com>
2020-07-29 08:03:46 +0100

    oops

--- loadmods.py
+++ loadmods.py
@@ -14,7 +14,7 @@ mod_name = str(obj['name'])
 print("name: " + mod_name)
 
 try:
-    for filename in glob.glob(r"C:\Users\giddy\AppData\Roaming\Factorio\mods\AutoFactorioGenerator_*" + mod_version):
+    for filename in glob.glob(r"C:\Users\giddy\AppData\Roaming\Factorio\mods\AutoFactorioGenerator_" + mod_version):
         shutil.rmtree(filename)
 except:
     print("no files to remove")
